fbc3cce85af9d53f6d15e8c52fd8270d947ffc12
Faizali H. Mulla <faizalihidayatulla.mulla@sjsu.edu>
2022-05-05 17:53:36 -0700

    Category Validation done

---
+++ Source/src/CategoryConfig.java
@@ -0,0 +1,5 @@
+public class CategoryConfig {
+    static Integer EssentialsCap = 3;
+    static Integer LuxuryCap = 4;
+    static Integer MiscCap = 6;
+}
--- Source/src/Validate.java
+++ Source/src/Validate.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import javax.print.DocFlavor.STRING;
@@ -30,12 +31,15 @@ public class Validate {
         boolean validFlag = true;
         String validationAvailabilityMessage = "";
         String validationQuantityMsg = "";
+        String validationCategoryMsg = "";
 
         Integer inventoryIndex;
         Integer inputIndex;
 
+        ArrayList<String> categoryList = new ArrayList<String>();
+
+        //CHECK IF ITEM AVAILABLE IN INVENTORY
         for (String element : inputList) {
-            //CHECK IF ITEM AVAILABLE IN INVENTORY
             if(inventoryList.contains(element) == false){
                 validationAvailabilityMessage = validationAvailabilityMessage + "|" + "Not contained: " + element;
                 validFlag = false;
@@ -43,22 +47,31 @@ public class Validate {
         }
 
         
-
+        //CHECK FOR THE QUANTITY AVAILABLE AND ALSO MAKE CATEGORY LIST
         for(Integer i=1; i<=input.size(); i++){
-
             if(inventoryList.contains(input.get(i)[0]) == true){
                 inventoryIndex = TableSearch.getIndex(inventoryList, input.get(i)[0]);
-            
+                categoryList.add(inventory.get(inventoryIndex)[1]);
                 if(Integer.parseInt(input.get(i)[1]) > Integer.parseInt(inventory.get(inventoryIndex)[2])){
-                validationQuantityMsg = validationQuantityMsg + "|" + "Quantity Issue:" + input.get(i)[0];
-                validFlag = false;
+                    validationQuantityMsg = validationQuantityMsg + "|" + "Quantity Issue:" + input.get(i)[0];
+                    validFlag = false;
+                }
             }
-
-            }
-            // System.out.println(inventory.get(inventoryIndex)[2]);
         }
         
-
+        //CHECK FOR THE CATEGORY CAP
+        Integer essentialsFrequency = Collections.frequency(categoryList, "Essentials");
+        if(essentialsFrequency > CategoryConfig.EssentialsCap){
+            System.out.println("category Issue");
+        }
+        Integer luxuryFrequency = Collections.frequency(categoryList, "Luxury");
+        if(luxuryFrequency > CategoryConfig.LuxuryCap){
+            System.out.println("category Issue");
+        }
+        Integer miscFrequency = Collections.frequency(categoryList, "Misc");
+        if(miscFrequency > CategoryConfig.MiscCap){
+            System.out.println("category Issue");
+        }
         System.out.println(validationAvailabilityMessage);
         System.out.println(validationQuantityMsg);
 
